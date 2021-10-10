c973a4d73f6934dc523ec2257b9d1ad39bd71350
pozafly <pozafly@gmail.com>
2021-10-10 12:01:14 +0900

    loader setting & webpack config setting

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   module: {
     rules: [
@@ -14,25 +14,32 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      // file-loader로 파일로 만들어줌.
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   loader: 'file-loader',
+      //   options: {
+      //     publicPath: './dist/src/assets',
+      //     name: '[name].[ext]?[hash]',
+      //   },
+      // },
+      // 용량 50000 미만인 녀석을 base64로 인코딩해서 인라인으로 넣어줌.
       {
         test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
+        loader: 'url-loader',
         options: {
           name: '[path][name].[ext]',
+          limit: 50000,
+          fallback: require.resolve('file-loader'),
         },
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
-      template: 'src/index.html',
-    }),
-  ],
   devServer: {
     port: 8080,
     open: true,
     hot: true,
+    // html 파일도 변경 시 hot reload
     watchContentBase: true,
     proxy: {
       '/api': {
@@ -41,4 +48,10 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      template: 'src/index.html',
+    }),
+  ],
 };
